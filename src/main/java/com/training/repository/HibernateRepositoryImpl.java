2ea6184a229590c6d2182eafac4d3e3311608722
jesus <jesusarenasm>
2018-12-27 15:22:41 -0600

    We just need to add the bean of PropertySourcePlaceHolder and specify via annotation the location of property file

--- src/main/java/com/training/repository/HibernateRepositoryImpl.java
+++ src/main/java/com/training/repository/HibernateRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.training.repository;
 
 import com.training.model.Customer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -10,12 +11,14 @@ import java.util.List;
 public class HibernateRepositoryImpl implements CustomerRepository {
 
     private List<Customer> customers = new ArrayList<>();
+    @Value("${username}")
+    private String username;
 
     @Override
     public List<Customer> findAll() {
         Customer customer = new Customer();
 
-        customer.setName("Jesus");
+        customer.setName(username);
         customer.setLastName("Sand");
 
         customers.add(customer);
