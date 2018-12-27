434e3254268ea2ebeb216ad18162724da8bf0556
jesus <jesusarenasm>
2018-12-27 15:08:30 -0600

    We need to define a place holder property in the application context.xml referencing to a .properties file and inject a property inside some bean and use inside the class that the bean refers to

--- src/main/java/com/training/repository/HibernateRepositoryImpl.java
+++ src/main/java/com/training/repository/HibernateRepositoryImpl.java
@@ -8,12 +8,17 @@ import java.util.List;
 public class HibernateRepositoryImpl implements CustomerRepository {
 
     private List<Customer> customers = new ArrayList<>();
+    private String username;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     @Override
     public List<Customer> findAll() {
         Customer customer = new Customer();
 
-        customer.setName("Jesus");
+        customer.setName(username);
         customer.setLastName("Sand");
 
         customers.add(customer);
