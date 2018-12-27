154986933b65a6bc5caefc475ddcb28482f44d29
jesus <jesusarenasm>
2018-12-27 11:48:07 -0600

    This is an example of java configuration, we just need to define the beans inside a class in wich we mark as a Configuration, we do not need the stereotypes

---
+++ src/main/java/com/training/AppConfig.java
@@ -0,0 +1,23 @@
+package com.training;
+
+import com.training.repository.CustomerRepository;
+import com.training.repository.HibernateRepositoryImpl;
+import com.training.service.CustomerService;
+import com.training.service.CustomerServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean("repository")
+    public CustomerRepository getCustomRepository() {
+        return new HibernateRepositoryImpl();
+    }
+
+    @Bean("customerService")
+    public CustomerService getCustomerService(CustomerRepository customerRepository) {
+        CustomerServiceImpl customerService = new CustomerServiceImpl();
+        customerService.setRepository(customerRepository);
+        return customerService;
+    }
+}
--- src/main/java/com/training/Client.java
+++ src/main/java/com/training/Client.java
@@ -2,18 +2,20 @@ package com.training;
 
 import com.training.model.Customer;
 import com.training.service.CustomerService;
-import com.training.service.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.List;
 
 public class Client {
 
     public static void main(String[] args) {
-        CustomerService service = new CustomerServiceImpl();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        CustomerService service = applicationContext.getBean("customerService", CustomerService.class);
 
         List<Customer> customers = service.findAll();
-        System.out.printf("Name: %s name again: %<s",customers.get(0).getName());
+        System.out.printf("Name: %s name again: %<s", customers.get(0).getName());
         System.out.println();
-        System.out.printf("Name: %s LastName:%s",customers.get(0).getName(), customers.get(0).getLastName());
+        System.out.printf("Name: %s LastName:%s", customers.get(0).getName(), customers.get(0).getLastName());
     }
 }
