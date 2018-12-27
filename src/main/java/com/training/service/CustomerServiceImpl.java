a143c359a81bf297625b0d3243b50446896b8c2d
jesus <jesusarenasm>
2018-12-27 08:31:25 -0600

    This contains an example of a constructor injection

--- src/main/java/com/training/service/CustomerServiceImpl.java
+++ src/main/java/com/training/service/CustomerServiceImpl.java
@@ -2,14 +2,16 @@ package com.training.service;
 
 import com.training.model.Customer;
 import com.training.repository.CustomerRepository;
-import com.training.repository.HibernateRepositoryImpl;
 
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
 
-    private CustomerRepository repository = new HibernateRepositoryImpl();
+    private CustomerRepository repository;
 
+    public CustomerServiceImpl(CustomerRepository repository) {
+        this.repository = repository;
+    }
 
     @Override
     public List<Customer> findAll() {
