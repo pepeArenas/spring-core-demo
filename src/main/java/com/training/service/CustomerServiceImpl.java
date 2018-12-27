331b61aefb5e42d53a482c5c13e8ff9335972073
jesus <jesusarenasm>
2018-12-27 13:37:30 -0600

    Also we can use the enum ConfigurableBeanFactory in order to specify the scope

--- src/main/java/com/training/service/CustomerServiceImpl.java
+++ src/main/java/com/training/service/CustomerServiceImpl.java
@@ -3,13 +3,14 @@ package com.training.service;
 import com.training.model.Customer;
 import com.training.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service("customerService")
-@Scope("singleton")
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class CustomerServiceImpl implements CustomerService {
 
     private CustomerRepository repository;
